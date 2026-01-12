d1997606d4a01e4914597266c34378904626c4aa
DEGISA74 <wm11tr@gmail.com>
2026-01-12 23:08:30 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -290,7 +290,7 @@ final_bist100_list = priority_bist_indices + raw_bist_stocks
 ASSET_GROUPS = {
     "S&P 500": final_sp500_list,
     "NASDAQ-100": raw_nasdaq,
-    "BIST 300": final_bist100_list,
+    "BIST İLK 300": final_bist100_list,
     "KRİPTO-TOP 25": final_crypto_list
 }
 INITIAL_CATEGORY = "S&P 500"
@@ -3781,5 +3781,6 @@ with col_right:
                     sym = row["Sembol"]
                     with cols[i % 2]:
                         if st.button(f"🚀 {row['Skor']}/7 | {row['Sembol']} | {row['Setup']}", key=f"r2_b_{i}", use_container_width=True): on_scan_result_click(row['Sembol']); st.rerun()
+
 
 
