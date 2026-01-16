0d1239d1d5e63c0f537282c2276b9ce7f650dd56
DEGISA74 <wm11tr@gmail.com>
2026-01-16 22:47:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4090,23 +4090,3 @@ with col_right:
                     sym = row["Sembol"]
                     with cols[i % 2]:
                         if st.button(f"🚀 {row['Skor']}/7 | {row['Sembol']} | {row['Setup']}", key=f"r2_b_{i}", use_container_width=True): on_scan_result_click(row['Sembol']); st.rerun()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
